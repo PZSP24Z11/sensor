180526492bbd5898a224de0696b82ce6c16542d0
doaads <nowobart@gmail.com>
2024-12-10 23:56:03 +0100

    feat: full SENSORREQ implementation

---
+++ nucleo-f439zi-sensor/utils.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DELIMETER '%'
+
+/* Formatting MAC addresses to a human-readable form */
+void format_mac(char *buf, uint8_t *addr, uint8_t len) {
+	char hex_buf[3];
+	for (size_t i = 0; i < len; ++i) {
+		sprintf(hex_buf, "%x", addr[i]);
+		if (addr[i] < 16) {
+			buf[i * 3] = '0';
+			memcpy(&buf[i * 3 + 1], hex_buf, 1);
+		} else {
+			memcpy(&buf[i * 3], hex_buf, 2);
+		}
+		buf[i * 3 + 2] = ':';
+	}
+
+	buf[len * 3 - 1] = (char)0;
+}
+
+int format_packet(char *out_buf, uint8_t buf_num, char *bufs[]) {
+	uint8_t offset = 0;
+	for (size_t i = 0; i < buf_num; ++i) {
+		strncpy(&out_buf[offset], bufs[i], strlen(bufs[i]));
+		offset += strlen(bufs[i]);
+		out_buf[offset] = '%';
+		offset++;
+	}
+	out_buf[offset - 1] = (char)0;
+	return 0;
+
+}
+
