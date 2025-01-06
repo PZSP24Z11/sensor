9245d00bcdec7a4f87653a59cfd9adf77835010f
maciej <maciej.scheffer@wp.pl>
2025-01-06 20:55:54 +0100

    login and register views

--- sensor-app/frontend/views.py
+++ sensor-app/frontend/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
